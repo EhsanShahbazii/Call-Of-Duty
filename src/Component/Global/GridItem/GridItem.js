7146ea98ef010b2a5fa1d7c080663beae0940fbc
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-11 17:39:26 +0430

    add image and finish grid but not complete

--- src/Component/Global/GridItem/GridItem.js
+++ src/Component/Global/GridItem/GridItem.js
@@ -1,18 +1,18 @@
 import './GridItem.css';
 
-export default function GridItem() {
+export default function GridItem({image, logo, text}) {
   return (
     <>
       <div className="container-gridItem">
         <div className="image-tainer">
-        <img src="./images/main page/grid-pics-part/p1.jpg" alt="grid item" />
+        <img src={image} alt="grid item" />
         </div>
           <div className="item-details">
             <div className='logo-images'>
-              <img src="./images/main page/logos-part/codm-white.png" alt="logo icon" />
+              <img src={logo} alt="logo icon" />
             </div>
             <div className='text-part'>
-              <span>Enough is never enough. Enough is never enough. Enough is never enough.</span>
+              <span>{text}</span>
             </div>
           </div>
       </div>
--- src/Component/Main/Grid/Grid.js
+++ src/Component/Main/Grid/Grid.js
@@ -3,6 +3,14 @@ import Button from './../../Global/Button/Button';
 import './Grid.css';
 
 export default function Grid() {
+
+  const dataList = [
+    {image: './images/main page/grid-pics-part/6.15-hub-s4-keyart-tout-small.jpg', logo: './images/main page/logos-part/vg-logo-white-logo.png', text: 'enough is never enough. play season four now.'},
+    {image: './images/main page/grid-pics-part/6.21-hub-s4-wz-sm-tout.jpg', logo: './images/main page/logos-part/wz-logo.png', text: 'go all-in and reap the rewards'},
+    {image: './images/main page/grid-pics-part/6.21-hub-s4-bp-sm-tout.jpg', logo: './images/main page/logos-part/vg-logo-white-logo.png', text: 'get access to all 100 tiers of battle pass content'},
+    {image: './images/main page/grid-pics-part/cq5dam.thumbnail.319.319(8).jpg', logo: './images/main page/logos-part/codm-white.png', text: 'pilot the new jackal in call of duty: mobile season 6-to the skies'}
+  ]
+
   return (
     <>
         <div className="container-grid-part">
@@ -13,10 +21,10 @@ export default function Grid() {
               <Button text={"Lean More"} />
               </div>
             </div>
-            <GridItem />
-            <GridItem />
-            <GridItem />
-            <GridItem />
+            <GridItem {...dataList[0]} />
+            <GridItem {...dataList[1]} />
+            <GridItem {...dataList[2]} />
+            <GridItem {...dataList[3]} />
         </div>
     </>
   )
