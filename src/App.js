5d347c9652be244fd868a1fea0db6dff05022045
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-12 11:30:35 +0430

    add arrows

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from './Component/Header/NavBar';
 import Grid from './Component/Main/Grid/Grid';
 import Landing from './Component/Main/Landing/Landing';
+import Slider from './Component/Main/Slider/Slider';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <NavBar />
       <Landing />
       <Grid />
+      <Slider />
     </>
   )
 }
--- src/Component/Main/Slider/Slider.js
+++ src/Component/Main/Slider/Slider.js
@@ -3,6 +3,22 @@ import './Slider.css';
 export default function Slider() {
   return (
     <>
+      <div className="container-slider">
+        <div className='header-detail'>
+
+        </div>
+        <div className='slider-main'>
+          <div className='left-arrow sl'>
+            <img src=".\icons\icons8-arrow-91.png" alt="arrow icon" />
+          </div>
+          <div className='slider-tainer'>
+
+          </div>
+          <div className='right-arrow sl'>
+            <img src=".\icons\icons8-arrow-91.png" alt="arrow icon" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
