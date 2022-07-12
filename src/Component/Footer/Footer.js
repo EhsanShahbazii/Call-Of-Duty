869c644f00a259f257275f37ea81384b937a4449
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-12 21:33:24 +0430

    add footer js file

---
+++ src/Component/Footer/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <>
+    <div className="container-footer">
+        <div className='footer-nav'>
+            <ul>
+                <li><a href="#">Legal</a></li>
+                <li><a href="#">Term of use</a></li>
+                <li><a href="#">Privacy policy</a></li>
+                <li><a href="#">cookie policy</a></li>
+                <li><a href="#">support</a></li>
+            </ul>
+        </div>
+        <div className='footer-icons'>
+
+        </div>
+        <div className='footer-detail'>
+
+        </div>
+    </div>
+    </>
+  )
+}
