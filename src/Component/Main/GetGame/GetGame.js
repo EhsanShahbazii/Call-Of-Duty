9cd6878acb7032a3fa308a77e2b5c2c69d9ee6f1
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-12 16:51:06 +0430

    Add image and text

--- src/Component/Main/GetGame/GetGame.js
+++ src/Component/Main/GetGame/GetGame.js
@@ -2,15 +2,23 @@ import './GetGame.css';
 import GameItem from '../../Global/GameItem/GameItem';
 
 export default function GetGame() {
+
+  const gameData = [
+    {image: './images/main page/get-the-game-part/COD-Mobile-x-Girls-Frontline.jpg', text: 'blockbuster campion multiplayer and zombie'},
+    {image: './images/main page/get-the-game-part/callofdutycoldwar.jpg', text: 'the iconic modern warfare ii series is back'},
+    {image: './images/main page/get-the-game-part/578050-call-of-duty-season-one-extended.jpg', text: 'free to play battle royale and more'},
+    {image: './images/main page/get-the-game-part/6ed05ced-c49a-416b-b1fa-289898162811.jpeg', text: 'the thrill of call of duty on the go'}
+  ]
+
   return (
     <>
     <div className="container-game">
       <h1 className='title'>Get the game</h1>
       <div className='game-tainer'>
-        <a href="#"><GameItem image='./images/main page/get-the-game-part/COD-Mobile-x-Girls-Frontline.jpg' text=' lorem epsun lorem epsun lorem epsun' /></a>
-        <a href="#"><GameItem image='./images/main page/get-the-game-part/COD-Mobile-x-Girls-Frontline.jpg' text=' lorem epsun lorem epsun lorem epsun' /></a>
-        <a href="#"><GameItem image='./images/main page/get-the-game-part/COD-Mobile-x-Girls-Frontline.jpg' text=' lorem epsun lorem epsun lorem epsun' /></a>
-        <a href="#"><GameItem image='./images/main page/get-the-game-part/COD-Mobile-x-Girls-Frontline.jpg' text=' lorem epsun lorem epsun lorem epsun' /></a>
+        <a href="#"><GameItem {...gameData[0]} /></a>
+        <a href="#"><GameItem {...gameData[1]} /></a>
+        <a href="#"><GameItem {...gameData[2]} /></a>
+        <a href="#"><GameItem {...gameData[3]} /></a>
       </div>
     </div>
     </>
