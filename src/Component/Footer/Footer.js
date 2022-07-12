320a58580cfd9331b06ff7563c91a08243e1142c
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-12 22:07:33 +0430

    finish footer main

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,7 +3,20 @@ import './Footer.css';
 
 export default function Footer() {
 
-    
+    const iconData = [
+        {image: './images/general/footer-part/atvi-shanghai-logo.png'},
+        {image: './images/general/footer-part/activision-logo.svg'},
+        {image: './images/general/footer-part/beenox-logo.png'},
+        {image: './images/general/footer-part/toys-for-bob-logo.svg'},
+        {image: './images/general/footer-part/demonware-logo.png'},
+        {image: './images/general/footer-part/digital-legends-logo.png'},
+        {image: './images/general/footer-part/high-moon-logo.png'},
+        {image: './images/general/footer-part/raven-logo.png'},
+        {image: './images/general/footer-part/activision-logo.svg'},
+        {image: './images/general/footer-part/solid-state-logo.png'},
+        {image: './images/general/footer-part/infinity-ward-logo.svg'},
+        {image: './images/general/footer-part/treyarch-logo.svg'}
+    ]
 
   return (
     <>
@@ -23,18 +36,18 @@ export default function Footer() {
         </div>
         <div className='footer-icons'>
             <div className='left-icons'>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>
-            <Icon image={'./images/general/footer-part/atvi-shanghai-logo.png'}/>   
+            <Icon {...iconData[0]} />
+            <Icon {...iconData[1]} />
+            <Icon {...iconData[2]} />
+            <Icon {...iconData[3]} />
+            <Icon {...iconData[4]} />
+            <Icon {...iconData[5]} />
+            <Icon {...iconData[6]} />
+            <Icon {...iconData[7]} />
+            <Icon {...iconData[8]} />
+            <Icon {...iconData[9]} />
+            <Icon {...iconData[10]} />
+            <Icon {...iconData[11]} />   
             </div>
             <div className='right-icons'>
             <img src="./images/general/footer-part/esrb-privacy.jpg" alt="logo" className='img-icon2' /> 
@@ -42,7 +55,7 @@ export default function Footer() {
             </div>
         </div>
         <div className='footer-detail'>
-            <span>© 2022 Activision Publishing, Inc. ACTIVISION, CALL OF DUTY, CALL OF DUTY LEAGUE, CALL OF DUTY MODERN WARFARE, MODERN WARFARE, CALL OF DUTY BLACK OPS, CALL OF DUTY WARZONE, WARZONE and CALL OF DUTY VANGUARD are trademarks of Activision Publishing, Inc. All other trademarks and trade names are the property of their respective owners.</span>
+            <span>© 2022 Activision Publishing, Inc. ACTIVISION, CALL OF DUTY, CALL OF DUTY LEAGUE, CALL OF DTY MODERN WARFARE, MODERN WARFARE, CALL OF DUTY BLACK OPS, CALL OF DUTY WARZONE, WARZONE and CLL OF DUTY VANGUARD are trademarks of Activision Publishing, Inc. All other trademarks and tade names are the property of their respective owners.</span>
         </div>
     </div>
     </>
