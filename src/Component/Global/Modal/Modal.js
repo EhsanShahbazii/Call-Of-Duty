7051680098dfa5823b0e3a39aa5fa16e0045ee3a
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-13 17:04:55 +0430

    Add hover and modal

--- src/Component/Global/Modal/Modal.js
+++ src/Component/Global/Modal/Modal.js
@@ -1,9 +1,9 @@
 import './Modal.css';
 
-export default function Modal() {
+export default function Modal({active}) {
   return (
     <>
-    <div className="modal-tainer">
+    <div className={["modal-tainer", "active"].join(' ')} >
         <div className='img-login'>
             <img src="./images/general/modal-part/call-of-duty-man-with-guns.jpg" alt="login images" />
         </div>
@@ -14,11 +14,17 @@ export default function Modal() {
             <span>SIGN IN TO YOUR ACCOUNT</span>
             </div>
             <form className='login-form'>
+                <div className="username-part">
                 <label>Email Address</label>
                 <input type="email" className='userName-login' />
+                </div>
+                <div className="password-part">
                 <label>Password</label>
                 <input type="password"className='password-login' />
+                </div>
+                <span className='error-span'>please fill the email and passowrd</span>
                 <button className='submit-btn'>Sign in</button>
+                <img src="./images/general/footer-part/digital-legends-logo.png" alt="icons" />
             </form>
         </div>
     </div>
