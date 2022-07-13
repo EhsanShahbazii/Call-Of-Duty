5563fe7b087956d9b2c93fe15df6be0400b0f106
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2022-07-14 01:02:14 +0430

    add modal js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ export default function App() {
   return (
     <>
       <NavBar />
-      <Modal />
       <Landing />
       <Grid />
       <Slider />
--- src/Component/Header/NavBar.js
+++ src/Component/Header/NavBar.js
@@ -1,39 +1,62 @@
 import './NavBar.css';
+import Modal from '../Global/Modal/Modal';
+import React from 'react';
 
-export default function NavBar() {
-  return (
-    <>
-        <div className="container">
-            <div className='left-box'>
-                <div className='logo'>
-                    <img src="./images/general/header-nav/cod-logo-white.svg" alt="call of duty logo"/>
+export default class NavBar extends React.Component {
+
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            signIn: false
+        }
+
+        this.signInHandler = this.signInHandler.bind(this)
+    }
+
+    signInHandler () {
+        this.setState({
+            signIn: !this.state.signIn
+        })
+    }
+
+    render () {
+        return (
+            <>
+            {this.state.signIn ? <Modal active={"active"} /> : <Modal />}
+            <div className="container">
+                <div className='left-box'>
+                    <div className='logo'>
+                        <img src="./images/general/header-nav/cod-logo-white.svg" alt="call of duty logo"/>
+                    </div>
+                    <nav>
+                        <ul className='nav-list'>
+                            <li><a href="#">GAMES</a></li>
+                            <li><a href="#">NEWS</a></li>
+                            <li><a href="#">ESPORTS</a></li>
+                            <li><a href="#">SUPPORT</a></li>
+                            <li><a href="#">SHOP</a></li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
-                    <ul className='nav-list'>
-                        <li><a href="#">GAMES</a></li>
-                        <li><a href="#">NEWS</a></li>
-                        <li><a href="#">ESPORTS</a></li>
-                        <li><a href="#">SUPPORT</a></li>
-                        <li><a href="#">SHOP</a></li>
-                    </ul>
-                </nav>
+                <div className='right-box'>
+                    <div className='log-part'>
+                        <a href="#" onClick={this.signInHandler}>Sign in</a>
+                        <b>|</b>
+                        <a href="#">SIGN UP</a>
+                    </div>
+                    <div className='shield-part'>
+                        <img src="./images/general/header-nav/shield_icon_no_drop.png" alt="shield icon" />
+                        MY CALL OF DUTY
+                    </div>
+                    <div className='pre-part'>
+                        PRE-ORDER-MWII
+                    </div>
+                    <img src="./images/general/header-nav/icons8-menu-48.png" alt="hamburger menu" className='ham-menu' />
+                </div>
             </div>
-            <div className='right-box'>
-                <div className='log-part'>
-                    <a href="#">Sign in</a>
-                    <b>|</b>
-                    <a href="#">SIGN UP</a>
-                </div>
-                <div className='shield-part'>
-                    <img src="./images/general/header-nav/shield_icon_no_drop.png" alt="shield icon" />
-                    MY CALL OF DUTY
-                </div>
-                <div className='pre-part'>
-                    PRE-ORDER-MWII
-                </div>
-                <img src="./images/general/header-nav/icons8-menu-48.png" alt="hamburger menu" className='ham-menu' />
-            </div>
-        </div>
-    </>
-  )
+            </>
+        )
+    }
 }
